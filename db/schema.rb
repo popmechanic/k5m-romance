ccf491458589ac8c37dd3f0d2ce114218d3d4466
Zach Rose <zachrose@gmail.com>
2012-05-13 09:29:27 -0700

    Scaffolding for Questions.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120513162441) do
+ActiveRecord::Schema.define(:version => 20120513162754) do
 
   create_table "candidates", :force => true do |t|
     t.string   "name"
+    t.text     "text"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "questions", :force => true do |t|
     t.text     "text"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
