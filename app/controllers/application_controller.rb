96950d413d13d04dce5c1f095cf4c0ef4d91d284
Zach Rose <zachrose@gmail.com>
2012-05-13 15:49:05 -0700

    Session management operational.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,34 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def index
+    authenticate
   end
   
   def login
+    cookies[:token] = ""
+  end
+
+  def logout
+    cookies[:token] = ""
+    flash[:notice] = "Logged out"
+    redirect_to :action => "login"
+  end
+  
+  def authenticate
+    if params[:password] || !cookies[:token].empty?
+      password = params[:password] || cookies[:token]
+      if Shareholder.find_by_password password
+        cookies[:token] = password
+        return true
+      else
+        flash[:notice] = "Incorrect code"
+        redirect_to :action => "login"
+        return false
+      end
+    else
+      redirect_to :action => "login"
+      return false
+    end
   end
   
 end
