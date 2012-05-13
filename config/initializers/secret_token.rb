852416be74ea4248c4f2518e9ed2f8d94570f825
Zach Rose <zachrose@gmail.com>
2012-05-13 09:16:44 -0700

    Empty Rails 3.2.3 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+K5mRomance::Application.config.secret_token = '1ad20822dfaafc7ed792ca70b5562496a3151a840b8203b56ce0d551fbfe5359c56a48bba0760dae28404dada7994728e7542714157093379c33ac7fcf38c278'
